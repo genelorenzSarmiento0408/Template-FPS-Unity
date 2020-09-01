37d01c5b15afbb40dfad48d0a3bc2c8357b88380
genelorenzSarmiento0408 <69145401+genelorenzSarmiento0408@users.noreply.github.com>
2020-09-01 17:30:32 +0800

    Added c sharp project

---
+++ MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    //floats
+    public float mouseSpeed = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    void Start()
+    {
+        //state of the mouse
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        //Main code 
+        float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
+
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
